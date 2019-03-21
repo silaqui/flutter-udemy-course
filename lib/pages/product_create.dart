ababf8f68201f77a009db3ef3f99c3fa1a59c7c0
miwozniak <mikolaj.wozniak@apollogic.com>
2019-03-21 08:07:02 +0100

    8-114 refactor

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -21,56 +21,70 @@ class _ProductCreatePage extends State<ProductCreatePage> {
     return Container(
         margin: EdgeInsets.all(15.0),
         child: ListView(children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Title',
-            ),
-            onChanged: (String value) {
-              setState(() {
-                _title = value;
-              });
-            },
-          ),
-          TextField(
-            maxLines: 4,
-            decoration: InputDecoration(
-              labelText: 'Description',
-            ),
-            onChanged: (String value) {
-              setState(() {
-                _description = value;
-              });
-            },
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Price',
-            ),
-            onChanged: (String value) {
-              setState(() {
-                _price = double.parse(value);
-              });
-            },
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
+          _buildTitleTextField(),
+          _buildDescriptionTextField(),
+          _buildPriceTextField(),
+          SizedBox(height: 10.0),
           RaisedButton(
             color: Theme.of(context).accentColor,
             textColor: Colors.white,
             child: Text("Save"),
             onPressed: () {
-              final Map<String, dynamic> product = {
-                'title': _title,
-                'description': _description,
-                'price': _price,
-                'image': 'assets/bee.jpg'
-              };
-              widget.addProduct(product);
-              Navigator.pushReplacementNamed(context, '/products');
+              _submitForm(context);
             },
           )
         ]));
+  }
+
+  void _submitForm(BuildContext context) {
+    final Map<String, dynamic> product = {
+      'title': _title,
+      'description': _description,
+      'price': _price,
+      'image': 'assets/bee.jpg'
+    };
+    widget.addProduct(product);
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
+  TextField _buildPriceTextField() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Price',
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _price = double.parse(value);
+        });
+      },
+    );
+  }
+
+  TextField _buildDescriptionTextField() {
+    return TextField(
+      maxLines: 4,
+      decoration: InputDecoration(
+        labelText: 'Description',
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _description = value;
+        });
+      },
+    );
+  }
+
+  TextField _buildTitleTextField() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Title',
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _title = value;
+        });
+      },
+    );
   }
 }
