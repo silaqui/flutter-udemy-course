d86b5d9b9eec1efd637b5cd6061bddfdd687f2dc
Miki <aivi.pol@gmail.com>
2019-03-26 22:44:20 +0100

    9-149

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,55 +1,58 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/product.dart';
+import 'package:flutter_app/scoped-models/products.dart';
 import 'package:flutter_app/widgets/price_tag_widget.dart';
 import 'package:flutter_app/widgets/ui_elements/title_default.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class ProductPage extends StatelessWidget {
-  final String productName;
-  final String productImageUrl;
-  final double productPrice;
-  final String location = 'Unknown';
-  final String productDescription;
+  final int productIndex;
 
-  ProductPage(this.productName, this.productImageUrl, this.productPrice,
-      this.productDescription);
-
+  ProductPage(this.productIndex);
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-        onWillPop: (() {
-          Navigator.pop(context, false);
-          return Future.value(false);
-        }),
-        child: Scaffold(
+    return WillPopScope(onWillPop: (() {
+      Navigator.pop(context, false);
+      return Future.value(false);
+    }), child: ScopedModelDescendant<ProductsModel>(
+      builder: (BuildContext context, Widget child, ProductsModel model) {
+        final Product product = model.products[productIndex];
+        return Scaffold(
           appBar: AppBar(
             title: Text('Product detail'),
           ),
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Image.asset(productImageUrl),
+              Image.asset(product.image),
               Container(
                 padding: EdgeInsets.all(10),
                 child: Row(children: [
-                  Expanded(child:TitleDefault(productName)),
-                  PriceTag(productPrice.toString())
+                  Expanded(
+                      child: TitleDefault(product.title)),
+                  PriceTag(product.price.toString())
                 ]),
               ),
               Container(
                 padding: EdgeInsets.all(10.0),
-                child: Text('This bee is from: ' + location,
-                    style: TextStyle(color: Colors.grey,fontFamily: 'Oswald', fontSize: 20.0)),
+                child: Text('This bee is from: Minesota',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontFamily: 'Oswald',
+                        fontSize: 20.0)),
               ),
               Container(
                 padding: EdgeInsets.all(10.0),
-                child: Text(productDescription,
+                child: Text(product.description,
                     style: TextStyle(fontFamily: 'Oswald', fontSize: 20.0)),
               ),
             ],
           ),
-        ));
+        );
+      },
+    ));
   }
-
 }
