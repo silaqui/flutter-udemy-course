54c9a6f702359168595be2c633e1363956eb0dbc
Miki <aivi.pol@gmail.com>
2019-04-16 22:11:04 +0200

    14-213

---
+++ lib/widgets/form_inputs/location.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:map_view/map_view.dart';
+import '../helpers/ensure_visible.dart';
+
+class LocationInput extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _LocationInputState();
+  }
+}
+
+class _LocationInputState extends State<LocationInput>{
+  final FocusNode _addressInputFocusNode = FocusNode();
+
+
+  @override
+  void initState() {
+    _addressInputFocusNode.addListener(_updateLocation);
+    super.initState();
+  }
+
+
+  @override
+  void dispose() {
+    _addressInputFocusNode.removeListener(_updateLocation);
+    super.dispose();
+  }
+
+  void _updateLocation(){
+
+  }
+
+  @override
+    Widget build(BuildContext context) {
+    return Column (children: <Widget>[
+      EnsureVisibleWhenFocused(
+        focusNode: _addressInputFocusNode,
+        child: TextFormField(
+                focusNode: _addressInputFocusNode,
+
+      ),)
+    ],);
+  }
+}
