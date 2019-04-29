75a10ce4fe2b44203c5f05d0239dc64bc0d19886
miwozniak <mikolaj.wozniak@apollogic.com>
2019-04-29 08:36:16 +0200

    14-214

--- lib/pages/product_edit.dart
+++ lib/pages/product_edit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/product.dart';
 import 'package:flutter_app/scoped-models/main.dart';
+import 'package:flutter_app/widgets/form_inputs/location.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class ProductEditPage extends StatefulWidget {
@@ -54,6 +55,8 @@ class _ProductEditPage extends State<ProductEditPage> {
                   _buildTitleTextFormField(product),
                   _buildDescriptionTextFormField(product),
                   _buildPriceTextFormField(product),
+                  SizedBox(height: 10.0),
+                  LocationInput(),
                   SizedBox(height: 10.0),
                   _buildSubmitButton()
                 ]))));
--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -2,23 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:map_view/map_view.dart';
 import '../helpers/ensure_visible.dart';
 
-class LocationInput extends StatefulWidget{
+class LocationInput extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _LocationInputState();
   }
 }
 
-class _LocationInputState extends State<LocationInput>{
+class _LocationInputState extends State<LocationInput> {
   final FocusNode _addressInputFocusNode = FocusNode();
 
+  Uri _staticManUri;
 
   @override
   void initState() {
     _addressInputFocusNode.addListener(_updateLocation);
+    getStaticMap();
     super.initState();
   }
-
 
   @override
   void dispose() {
@@ -26,19 +27,36 @@ class _LocationInputState extends State<LocationInput>{
     super.dispose();
   }
 
-  void _updateLocation(){
-
+  //async for future use, not required now
+  void getStaticMap() async {
+    final StaticMapProvider staticMapProvider =
+        StaticMapProvider('AIzaSyA4YHhJUn3UNsoQ6ml4g_WK59sGms5DZ7A');
+    final Uri mapUrl = staticMapProvider.getStaticUriWithMarkers(
+        [Marker('Position', 'Position', 41.22222, 2.15452)],
+        width: 500,
+        height: 300,
+        center: Location(41.22222, 2.15452),
+        maptype: StaticMapViewType.roadmap);
+    setState(() {
+      _staticManUri = mapUrl;
+    });
   }
 
-  @override
-    Widget build(BuildContext context) {
-    return Column (children: <Widget>[
-      EnsureVisibleWhenFocused(
-        focusNode: _addressInputFocusNode,
-        child: TextFormField(
-                focusNode: _addressInputFocusNode,
+  void _updateLocation() {}
 
-      ),)
-    ],);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        EnsureVisibleWhenFocused(
+          focusNode: _addressInputFocusNode,
+          child: TextFormField(
+            focusNode: _addressInputFocusNode,
+          ),
+        ),
+        SizedBox(height: 10.0),
+        Image.network(_staticManUri.toString())
+      ],
+    );
   }
 }
