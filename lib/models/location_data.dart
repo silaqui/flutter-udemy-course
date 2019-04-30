813fcc71054b82194f53ab2d49cf2f79871e5c9b
miwozniak <mikolaj.wozniak@apollogic.com>
2019-04-30 07:54:40 +0200

    14-217

---
+++ lib/models/location_data.dart
@@ -0,0 +1,10 @@
+class LocationDate {
+
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  LocationDate(this.latitude, this.longitude, this.address);
+
+
+}
--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -3,6 +3,7 @@ import 'package:map_view/map_view.dart';
 import '../helpers/ensure_visible.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import '../../models/location_data.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -15,7 +16,7 @@ class _LocationInputState extends State<LocationInput> {
   final FocusNode _addressInputFocusNode = FocusNode();
   final TextEditingController _addressInputController =
       new TextEditingController();
-
+  LocationDate _locationData;
   Uri _staticManUri;
 
   @override
@@ -32,6 +33,8 @@ class _LocationInputState extends State<LocationInput> {
 
   void getStaticMap(String address) async {
     if (address.isEmpty) {
+      _staticManUri = null;
+//      _locationData = null;
       return;
     }
     final Uri uri = Uri.https('maps.googleapis.com', '/maps/api/geocode/json',
@@ -49,17 +52,18 @@ class _LocationInputState extends State<LocationInput> {
     print(lat);
     final double lng = decodedResponse['results'][0]['geometry']['location']['lng'];
     print(lng);
+    _locationData = LocationDate(lat,lng,formattedAddress);
 
     final StaticMapProvider staticMapProvider =
         StaticMapProvider('AIzaSyA4YHhJUn3UNsoQ6ml4g_WK59sGms5DZ7A');
     final Uri mapUrl = staticMapProvider.getStaticUriWithMarkers(
-        [Marker('Position', 'Position', lat, lng)],
+        [Marker('Position', 'Position', _locationData.latitude, _locationData.longitude)],
         width: 500,
         height: 300,
         center: Location(lat, lng),
         maptype: StaticMapViewType.roadmap);
     setState(() {
-      _addressInputController.text = formattedAddress;
+      _addressInputController.text = _locationData.address;
       _staticManUri = mapUrl;
     });
   }
@@ -79,11 +83,16 @@ class _LocationInputState extends State<LocationInput> {
           child: TextFormField(
             focusNode: _addressInputFocusNode,
             controller: _addressInputController,
+            validator: (String value){
+              if(_locationData == null || value.isEmpty){
+                return 'No valid location found';
+              }
+            },
             decoration: InputDecoration(labelText: 'Address'),
           ),
         ),
         SizedBox(height: 10.0),
-        Image.network(_staticManUri.toString())
+        _staticManUri!=null ? Image.network(_staticManUri.toString()): SizedBox()
       ],
     );
   }
