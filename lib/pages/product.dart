46e9e5bd4df88055934c6945c25756fd93a60ad9
miwozniak <mikolaj.wozniak@apollogic.com>
2019-05-14 09:47:45 +0200

    14-230

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,11 +4,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/models/product.dart';
 import 'package:flutter_app/widgets/price_tag_widget.dart';
 import 'package:flutter_app/widgets/ui_elements/title_default.dart';
+import 'package:map_view/map_view.dart';
 
 class ProductPage extends StatelessWidget {
   final Product product;
 
   const ProductPage(this.product);
+
+  void _showMap() {
+    final markers = <Marker>[
+      Marker('position', 'Position', product.location.latitude,
+          product.location.longitude)
+    ];
+    final mapView = MapView();
+    final cameraPosition = CameraPosition(
+        Location(product.location.latitude, product.location.longitude), 14.0);
+    mapView.show(
+        MapOptions(
+            initialCameraPosition: cameraPosition,
+            mapViewType: MapViewType.normal,
+            title: 'Bee location'),
+        toolbarActions: [ToolbarAction('Close', 1)]);
+    mapView.onToolbarAction.listen((int i) {
+      if (i == 1) {
+        mapView.dismiss();
+      }
+    });
+    mapView.onMapReady.listen((_){
+      mapView.setMarkers(markers);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +58,15 @@ class ProductPage extends StatelessWidget {
                 ]),
               ),
               Container(
-                padding: EdgeInsets.all(10.0),
-                child: Text('This bee is from: Minesota',
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontFamily: 'Oswald',
-                        fontSize: 20.0)),
-              ),
+                  padding: EdgeInsets.all(10.0),
+                  child: GestureDetector(
+                    onTap: _showMap,
+                    child: Text(product.location.address,
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontFamily: 'Oswald',
+                            fontSize: 20.0)),
+                  )),
               Container(
                 padding: EdgeInsets.all(10.0),
                 child: Text(product.description,
