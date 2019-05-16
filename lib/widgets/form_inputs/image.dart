440a487dde11eba24f48b437a9c5cc2583f19d9f
Miki <aivi.pol@gmail.com>
2019-05-16 21:39:58 +0200

    514-234

--- lib/widgets/form_inputs/image.dart
+++ lib/widgets/form_inputs/image.dart
@@ -8,19 +8,45 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  void _openImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 150.0,
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                Text("Pick an Image",style: TextStyle(fontWeight:FontWeight.bold),),
+                SizedBox(height: 10.0),
+                FlatButton(
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text("Use Camera"),
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text("Use Galery"),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     final buttonColor = Theme.of(context).accentColor;
     return Column(
       children: <Widget>[
         OutlineButton(
-          borderSide:
-              BorderSide(color: buttonColor, width: 2.0),
-          onPressed: () {},
+          borderSide: BorderSide(color: buttonColor, width: 2.0),
+          onPressed: () {_openImagePicker(context);},
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Icon(Icons.camera_alt,color: buttonColor),
+              Icon(Icons.camera_alt, color: buttonColor),
               SizedBox(width: 5.0),
               Text(
                 'Add Image',
