b57b0eaffcb8bbcf8f73f74340968ee9d65efe14
miwozniak <mikolaj.wozniak@apollogic.com>
2019-05-21 10:28:46 +0200

    15-241

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const Busboy = require('busboy');
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
+const fbAdmin = require('firebase-admin');
+const uuid = require('uuid/v4');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,6 +13,17 @@ const fs = require('fs');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const gcconfig = {
+    projectId: 'flutterudemycourse',
+    keyFilename: 'flutter-products.json'
+};
+
+const gcs = require('@google-cloud/storage')(gcconfig);
+
+fbAdmin.initializeApp({
+    credential: fbAdmin.credential.cert(require('./flutter-products.json'))
+});
 
 exports.storeImage = functions.https.onRequest((req, res) => {
     return cors(req,res,()=>{
@@ -38,8 +51,51 @@ exports.storeImage = functions.https.onRequest((req, res) => {
         })
 
         busboy.on('finish',()=>{
+            const bucket = gcs.bucket('flutterudemycourse.appspot.com');
+            cont id = uuid();
+            let imagePath = '/images/'+id+'-'+uploadData.name;
+            if(oldImagePath){
+                imagePath = oldImagePath;
+            }
 
-        })
-
+            return fbAdmin.auth().verifyIdToken(idToken)
+                .then(decotedToken =>{
+                    return bucket.upload(uploadData.filePath,{
+                            uploadType: 'media',
+                            destination: imagePath,
+                            metadata:{
+                                metadata:{
+                                        contentType: uploadData.type,
+                                        firebaseStorageDownloadToken: id
+                                }
+                            }
+                        });
+                })
+                .then(()=>{
+                    return res.status(201).json({
+                        imageUrl:
+                        'https://firebasestorage.googleapis.com/v0/b' +
+                        bucket.name +
+                        '/o/' +
+                        encodeURIComponet(imagePath) +
+                        '?alt=media&token=' +
+                        id,
+                        imagePath: imagePath
+                    });
+                })
+                .catch(error => {
+                    return res.status(401).json({error: 'Niet'})
+                });
+        });
+        return busboy.end(req.rawBody);
     });
 });
+
+
+
+
+
+
+
+
+
