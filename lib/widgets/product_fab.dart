fa555b0024aac78854a01d69512de16f34e3a6c1
miwozniak <mikolaj.wozniak@apollogic.com>
2019-06-04 08:33:49 +0200

    257

--- lib/widgets/product_fab.dart
+++ lib/widgets/product_fab.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:flutter_app/models/product.dart';
 import 'package:flutter_app/scoped-models/main.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProductFab extends StatefulWidget {
   final Product product;
@@ -14,57 +15,94 @@ class ProductFab extends StatefulWidget {
   }
 }
 
-class _ProductFabState extends State<ProductFab> {
+class _ProductFabState extends State<ProductFab> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<MainModel>(
-        builder: (BuildContext context, Widget child, MainModel model) {
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
+      builder: (BuildContext context, Widget child, MainModel model) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
               height: 70,
               width: 56,
               alignment: FractionalOffset.topCenter,
-              child: FloatingActionButton(
-                backgroundColor: Theme.of(context).cardColor,
-                heroTag: 'contact',
-                mini: true,
-                onPressed: () {},
-                child: Icon(
-                  Icons.mail,
-                  color: Theme.of(context).accentColor,
+              child: ScaleTransition(
+                scale: CurvedAnimation(
+                    parent: _controller,
+                    curve: Interval(0.0, 1.0, curve: Curves.easeOut)),
+                child: FloatingActionButton(
+                  backgroundColor: Theme.of(context).cardColor,
+                  heroTag: 'contact',
+                  mini: true,
+                  onPressed: () async {
+                    final url = 'mailto:${widget.product.userEmail}';
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch!';
+                    }
+                  },
+                  child: Icon(
+                    Icons.mail,
+                    color: Theme.of(context).accentColor,
+                  ),
                 ),
-              )),
-          Container(
-            height: 70,
-            width: 56,
-            alignment: FractionalOffset.topCenter,
-            child: FloatingActionButton(
-              backgroundColor: Theme.of(context).cardColor,
-              mini: true,
-              heroTag: 'favorite',
-              onPressed: () {
-                model.toggleProductFavoriteStatus();
-              },
-              child: Icon(
-                  model.selectedProduct.isFavorite
-                      ? Icons.favorite
-                      : Icons.favorite_border,
-                  color: Theme.of(context).accentColor),
+              ),
             ),
-          ),
-          Container(
-            height: 70,
-            width: 56,
-            child: FloatingActionButton(
-              heroTag: 'options',
-              onPressed: () {},
-              child: Icon(Icons.more_vert),
+            Container(
+              height: 70,
+              width: 56,
+              alignment: FractionalOffset.topCenter,
+              child: ScaleTransition(
+                scale: CurvedAnimation(
+                    parent: _controller,
+                    curve: Interval(0.0, 1.0, curve: Curves.easeOut)),
+                child: FloatingActionButton(
+                  backgroundColor: Theme.of(context).cardColor,
+                  mini: true,
+                  heroTag: 'favorite',
+                  onPressed: () {
+                    model.toggleProductFavoriteStatus();
+                  },
+                  child: Icon(
+                      model.selectedProduct.isFavorite
+                          ? Icons.favorite
+                          : Icons.favorite_border,
+                      color: Theme.of(context).accentColor),
+                ),
+              ),
             ),
-          ),
-        ],
-      );
-    });
+            Container(
+              height: 70,
+              width: 56,
+              child: FloatingActionButton(
+                heroTag: 'options',
+                onPressed: () {
+                  if (_controller.isDismissed) {
+                    _controller.forward();
+                  } else {
+                    _controller.reverse();
+                  }
+                },
+                child: Icon(Icons.more_vert),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
