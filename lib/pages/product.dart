a2d9fed3782cf53ca2aed46f9f0b73e6fb582194
miwozniak <mikolaj.wozniak@apollogic.com>
2019-06-05 08:55:45 +0200

    262

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -44,22 +44,31 @@ class ProductPage extends StatelessWidget {
         return Future.value(false);
       }),
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Product detail'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Hero(tag: product.id, child: Image.network(product.image)),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Row(children: [
-                  Expanded(child: TitleDefault(product.title)),
-                  PriceTag(product.price.toString())
-                ]),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 256.0,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(product.title),
+                background: Hero(
+                    tag: product.id,
+                    child: Image.network(
+                      product.image,
+                      fit: BoxFit.cover,
+                    )),
               ),
-              Container(
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Row(children: [
+                    Expanded(child: TitleDefault(product.title)),
+                    PriceTag(product.price.toString())
+                  ]),
+                ),
+                Container(
                   padding: EdgeInsets.all(10.0),
                   child: GestureDetector(
                     onTap: _showMap,
@@ -68,14 +77,25 @@ class ProductPage extends StatelessWidget {
                             color: Colors.grey,
                             fontFamily: 'Oswald',
                             fontSize: 20.0)),
-                  )),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child: Text(product.description,
-                    style: TextStyle(fontFamily: 'Oswald', fontSize: 20.0)),
-              ),
-            ],
-          ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    product.description,
+                    style: TextStyle(fontFamily: 'Oswald', fontSize: 20.0),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing eit. Duis ultrices ut lectus a porttitor. Suspendisse feugiat mauris id scelerisque tempor. Sed a nisl lacus. Integer a eros id ex malesuada pellentesque. Etiam ultricies lacinia nulla, non iaculis orci hendrerit vitae. Donec est leo, tincidunt quis egestas sed, porta ac justo. Sed in sem mattis, rhoncus enim a, consequat mi. Proin odio justo, efficitur ac est a, tempus placerat orci. Quisque non sapien laoreet, luctus justo vitae, consequat neque. Suspendisse eros ipsum, tristique eget pharetra nec, posuere vitae orci. Vestibulum rutrum facilisis augue at semper. Vestibulum ultrices metus id ipsum faucibus tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis vel sem risus.",
+                    style: TextStyle(fontFamily: 'Oswald', fontSize: 20.0),
+                  ),
+                ),
+              ]),
+            ),
+          ],
         ),
         floatingActionButton: ProductFab(product),
       ),
